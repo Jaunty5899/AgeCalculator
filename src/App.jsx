f83ff49a0b17d88e15808673a362186a2829e8fd
Jaunty <jaunty5899@gmail.com>
2025-01-07 16:28:58 +0530

    date capture

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [date, setdate] = useState();
+
   return (
     <div className="container">
       <span id="title">Age Calculator</span>
       <form action="/">
         <label htmlFor="date">Enter your birth date:</label>
-        <input type="date" id="date" />
+        <input
+          type="date"
+          id="date"
+          value={date}
+          onChange={(e) => console.log(e.target.value)}
+        />
         <button type="submit">Calculate</button>
       </form>
     </div>
