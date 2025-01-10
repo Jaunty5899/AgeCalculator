7f462e76648eca6127f316d7364c52c11533d154
Jaunty <jaunty5899@gmail.com>
2025-01-10 15:45:03 +0530

    age calc complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import { DateTime } from "luxon";
 import "./App.css";
 
 function App() {
-  const [age, setAge] = useState(0);
-  let current = DateTime.now().c.year;
-  let DoB;
+  const [age, setAge] = useState({ years: 0, months: 0 });
+  let current = DateTime.now().c;
   return (
     <div className="container">
       <span id="title">Age Calculator</span>
@@ -15,13 +14,23 @@ function App() {
           type="date"
           id="date"
           onChange={(e) => {
-            DoB = DateTime.fromISO(e.target.value).c.year;
-            setAge(current - DoB);
+            setAge({
+              years: current.year - DateTime.fromISO(e.target.value).c.year,
+              months: -(
+                current.month - DateTime.fromISO(e.target.value).c.month
+              ),
+            });
           }}
         />
         {/* <button type="submit">Calculate</button> */}
       </form>
-      <span id="ageBlock">You are {age} years old.</span>
+      <span id="ageBlock">
+        You are{" "}
+        <strong>
+          {age.years} years {age.months} months
+        </strong>{" "}
+        old
+      </span>
     </div>
   );
 }
